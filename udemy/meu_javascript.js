1621e693cdb256e699768da8aa06f3b3b86185bd
joaopaulojpos <jpo.oliveiras@gmail.com>
2017-05-17 11:10:49 -0300

    iniciando os estudos em JavaScript

---
+++ udemy/meu_javascript.js
@@ -0,0 +1,7 @@
+// comentário de uma linha
+
+/* comentário de múltipas linhas
+Este tipo de comentário permite que várias
+linhas sejam comentadas */
+
+alert('Este é um teste de inclusão de js á partir de um arquivo externo');
